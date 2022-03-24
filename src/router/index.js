fd7ddc70fbef554f67d9a44b8fff24f9e571e187
cat214 <catou0214@gmail.com>
2022-03-25 02:52:28 +0900

    spa　ルーティング処理追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import About from '@/components/About.vue'
+import Works from '@/components/Works.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/hello',
-    //   name: 'Hello',
-    //   component: Hello
-    // },
-    // {
-    //   path: '/world',
-    //   name: 'World',
-    //   component: World
-    // }
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/works',
+      name: 'Works',
+      component: Works
+    }
   ]
 })
